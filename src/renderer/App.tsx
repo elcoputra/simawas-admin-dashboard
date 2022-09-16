6e00dfa97118d4c10de63f0e01863f3c358ea3e8
elcoputra <elcoputra94@gmail.com>
2022-09-16 10:37:50 +0700

    second

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -44,6 +44,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Hello />} />
+        <Route path="/About" element={<Hello />} />
       </Routes>
     </Router>
   );
